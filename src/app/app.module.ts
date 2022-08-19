efb6d6bf6bda0612c11c71169d6c0da7ab044a19
Cristian Pato <cristianpatosanchez88@gmail.com>
2022-08-19 10:20:57 +0200

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+
+// Tarea:
+// Crear un módulo llamado contadorModule.
+// declaraciones, y exportaciones 
+
+import { HeroesModule } from './heroes/heroes.module';
+import { contadorModule } from './contador/contador/contador.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    contadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
